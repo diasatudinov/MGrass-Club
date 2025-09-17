a646e99df1408191d0e54c4f56611f97b79cdbd5
Dias <dias.atudinov@gmail.com>
2025-09-17 17:56:07 +0500

    add helpers and loader view

---
+++ MGrass Club/MGrass Club/MCHelpers/ZZDeviceManager.swift
@@ -0,0 +1,19 @@
+//
+//  ZZDeviceManager.swift
+//  MGrass Club
+//
+//
+
+
+
+import UIKit
+
+class ZZDeviceManager {
+    static let shared = ZZDeviceManager()
+    
+    var deviceType: UIUserInterfaceIdiom
+    
+    private init() {
+        self.deviceType = UIDevice.current.userInterfaceIdiom
+    }
+}
