f8c7c7ae6bd98da9da7404393c189e786dc2b962
Dias <dias.atudinov@gmail.com>
2025-09-17 17:44:50 +0500

    app setup

---
+++ MGrass Club/MGrass Club/MGrass_ClubApp.swift
@@ -0,0 +1,16 @@
+//
+//  MGrass_ClubApp.swift
+//  MGrass Club
+//
+//
+
+import SwiftUI
+
+@main
+struct MGrass_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
